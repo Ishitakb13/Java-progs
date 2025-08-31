e82987c34f2b60e8f6834a037085f32d12493f41
Ishitakb13 <ishita.kb06@gmail.com>
2025-08-31 18:42:26 +0530

    uploaded campus courier

---
+++ Ebike.java
@@ -0,0 +1,15 @@
+package transport;
+
+public class Ebike extends Bicycle {
+	private int battery;
+	public Ebike(String id,int battery) {
+		super(id);
+		this.battery = battery;
+		System.out.println("EBike() constructor called");
+	}
+	public void deliver (String item, String place) {
+        System.out.println("Checking battery: " + battery + "%");
+        super.deliver(item,place);
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package main;
+import transport.*;
+public class Main {
+	 public static void main(String[] args) {
+	      
+	        Ebike e = new Ebike( "EB-101", 60 );
+	        e.deliver("Sandwich", "Library");
+	        Drone d = new Drone( "DR-1");
+	        d.deliver( "Notes", "ExamCell");  
+	        d.deliver( "USB", "ISE Block");
+	        double bill = d.cost( 5 );
+	        System.out.println("Drone delivery cost: Rs." +  bill);
+}
+}
